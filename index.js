de1b7faaad2ac310c64bea274c2808be8409aaa1
Kapil Tiwari <tiwarikapil216@gmail.com>
2024-05-24 09:07:51 +0530

    git

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let accountEmail='tiwari@gmail.com'
 var accountPassword= '1234'
 accountCity='jaipur'
 console.table([accountId,accountEmail,accountPassword,accountCity])
+console.log(accountPassword);
